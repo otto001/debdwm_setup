f7f09eb87414faa998841059a5978acbfeed5c88
otto001 <matteo.ludwig@gmail.com>
2021-04-23 08:04:44 +0200

    changed default sleep settings

--- setup-sleep.sh
+++ setup-sleep.sh
@@ -1,11 +1,13 @@
 if [ -z "$(ls -A /sys/class/power_supply)" ]; then
    IDLESEC="30min"
+   HIBERNATE="120min"
 else
    IDLESEC="10min"
+   HIBERNATE="60min"
 fi
 
 
-sudo sed -i "s/#HibernateDelaySec=.\+$/HibernateDelaySec=60min/" /etc/systemd/sleep.conf
+sudo sed -i "s/#HibernateDelaySec=.\+$/HibernateDelaySec=$HIBERNATE/" /etc/systemd/sleep.conf
 
 sudo sed -i "s/#IdleAction=.\+$/IdleAction=suspend-then-hibernate/" /etc/systemd/logind.conf
 sudo sed -i "s/#IdleActionSec=.\+$/IdleActionSec=$IDLESEC/" /etc/systemd/logind.conf
