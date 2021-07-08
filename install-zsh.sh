933461be72af36ea48e19c325a9e28c7ea21ba3c
otto001 <matteo.ludwig@gmail.com>
2021-07-08 21:16:23 +0200

    fix install-zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -10,16 +10,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p /tmp/zsh
 cd /tmp/zsh
 rm -Rf *
-tar -xzf $DIR/data/zsh/zsh-config.tar.gz
-if [[ ! -d /root/.oh-my-zsh ]]; then
-	sudo cp -r .oh-my-zsh /root/
-	sudo cp -r .zshrc /root/
+tar -xzf $DIR/data/zsh/oh-my-zsh.tar.gz
+
+if [[ ! -d /root/.zshrc ]]; then
+	sudo cp -ur /tmp/zsh/.oh-my-zsh /root/
+	sudo cp -ur $DIR/data/zsh/.zshrc /root/
 	sudo chown -R root:root /root/.oh-my-zsh /root/.zshrc
 fi
 
-if [[ ! -d $HOME/.oh-my-zsh ]]; then
-	cp -r .oh-my-zsh $HOME
-	cat .zshrc | sed "s,/root/,$HOME,g" | sed "s,root,$USER,g" > $HOME/.zshrc
+if [[ ! -d $HOME/.zshrc ]]; then
+	cat $DIR/data/zsh/.zshrc | sed "s,/root,$HOME,g" | sed "s,root,$USER,g" > /tmp/.zshrc
+	cp -ur /tmp/.zshrc $HOME/.zshrc
+	cp -ur /tmp/zsh/.oh-my-zsh $HOME
 	chown -R $1:$1 $HOME/.oh-my-zsh $HOME/.zshrc
 fi
+
 rm -Rf /tmp/zsh
