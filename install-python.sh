6a412ba61b4a376fe6eacc14de8e3aef626dea6a
otto001 <matteo.ludwig@gmail.com>
2021-09-19 21:47:16 +0200

    install-python autocomplete version

--- install-python.sh
+++ install-python.sh
@@ -11,10 +11,18 @@ V=$1
 
 sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
 
+if [[ $V =~ ^[0-9].[0-9]+$ ]]; then
+	if [[ ! -f "/tmp/python-versions.html" ]]; then
+		curl -q https://www.python.org/downloads/ > /tmp/python-versions.html
+	fi
+	V=$(cat /tmp/python.html | grep -oE "Python $V.[0-9]+" | awk 'NR==1 {print $2}')
+	echo "Chose version $V."
+fi
+
 cd /tmp
 curl -O https://www.python.org/ftp/python/$V/Python-$V.tar.xz
 tar -xf Python-$V.tar.xz
 cd Python-$V
 ./configure --enable-optimizations
-make -j 4
+make -j $(($(nproc)-1))
 sudo make altinstall
