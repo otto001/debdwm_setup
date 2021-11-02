c744219a332af97799eb116d943fc36b3bf201e5
otto001 <matteo.ludwig@gmail.com>
2021-11-03 00:38:38 +0100

    new desktop files for firefox

--- install-firefox.sh
+++ install-firefox.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 cd /tmp
 wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64" -O firefox.tar.bz2
 tar -xjf firefox.tar.bz2
 rm firefox.tar.bz2
 
+sudo rm -Rf /usr/local/lib/firefox
+sudo rm -f /usr/local/bin/firefox
 sudo mv firefox /usr/local/lib/firefox
 
-set +e
 sudo ln -s  /usr/local/lib/firefox/firefox /usr/local/bin/firefox
+cp -u $DIR/data/firefox/*.desktop ~/.local/share/applications/
